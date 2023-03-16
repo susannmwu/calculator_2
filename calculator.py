7ffea25a730ac1f5a126ff539ef6c0bb97d39b1f
Susan Wu <susann.wu@Susans-MacBook-Pro.local>
2023-03-15 17:23:18 -0700

    completed operations for pow, subtract and add

--- calculator.py
+++ calculator.py
@@ -12,20 +12,35 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 #question for user
 #     tokenize input
 #string split
-#         if the first token is "q":
-#             quit
-#break the loop here
-#calculation steps 
-#         else:
+#         if...
+#               elif...
 #             (decide which math function to call based on first token)
 #             if the first token is 'pow':
 #                   call the power function with the other two tokens
-
-#             (...etc.)
+#calculation steps 
+#         else:
+#the first token is "q":
+#             quit
+#break the loop here
 
 while True:
-   math_operations = input("What operations do you wish to use? ")
-   
-   tokens = math_operations.split(" ")
+    math_operations = input("What operations do you wish to use? ")
 
+#    operations_list = 'q add subtract multiply divid square cube pow mod'
+    operations_token = math_operations.split(" ")
+#lines 35 and 36 converts the items at position 1 and position 2 into intgers
+    operations_token[1] = int(operations_token[1])
+    operations_token[2] = int(operations_token[2])
+
+
+    if operations_token[0] == "pow":
+        print(power(operations_token[1], operations_token[2]))
+
+    elif operations_token[0] == "+":
+      print(add(operations_token[1], operations_token[2]))
+    
+    elif operations_token[0] == "-":
+       print(subtract(operations_token[1], operations_token[2]))
+ 
    
+      
